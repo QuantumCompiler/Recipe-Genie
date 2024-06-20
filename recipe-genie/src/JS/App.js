d5b45169afe6bdb209413a184723ca1527dc82ca
QuantumCompiler <tjlarrechea@gmail.com>
2024-06-19 22:30:39 -0700

    Initial Changes

---
+++ recipe-genie/src/JS/App.js
@@ -0,0 +1,16 @@
+import '../CSS/App.css';
+
+function App() {
+  return (
+    <div>
+      <title>Recipe Genie</title>
+      <div>
+        <h1 className='home-title'>Home</h1>
+        <h2 className='options-select'>Please Select One Of The Options Below</h2>
+      </div>
+    </div>
+  );
+}
+
+
+export default App;
