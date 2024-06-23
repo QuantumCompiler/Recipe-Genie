d08627c9cfe7402dc07ceb72fd4a5fb7554f9253
QuantumCompiler <tjlarrechea@gmail.com>
2024-06-23 16:52:22 -0700

    Index JS Update

--- recipe-genie/src/index.js
+++ recipe-genie/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './CSS/index.css';
 import App from './JS/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
