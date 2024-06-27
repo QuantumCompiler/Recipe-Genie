b532fdde5f423a2438ebbbc6e9a4ed6558c96b81
Tyler Hopkins <thop161@icloud.com>
2024-06-27 04:37:33 +0000

    Adding the initial API calls in Python with a test output .txt file and unittesting for the method.

--- recipe-genie/src/Python/spoonacularClient.py
+++ recipe-genie/src/Python/spoonacularClient.py
@@ -3,48 +3,55 @@ import os
 import pprint
 
 
-url = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/complexSearch"
+'''
+This is an example query for our input that is needed for the call to the API:
 
-querystring = {"query":"side salad","diet":"vegetarian","intolerances":"gluten","includeIngredients":"cheese,nuts","excludeIngredients":"eggs","instructionsRequired":"true","fillIngredients":"false","addRecipeInformation":"false","addRecipeInstructions":"false","addRecipeNutrition":"false","maxReadyTime":"45","ignorePantry":"true","sort":"max-used-ingredients","offset":"0","number":"10","limitLicense":"true"}
-
-headers = {
-	"x-rapidapi-key": "0969f52154mshf5d39e0b2d8cbf0p1af05bjsn7ac99719ef08",
-	"x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
+query = {
+    "ingredients":"eggs,flour,butter",
+    "number":5,
+    "limitLicense":False,
+    "ranking":1,
+    "ignorePantry":False
 }
 
-response = requests.get(url, headers=headers, params=querystring)
+'''
 
-print(response.json())
+#This is our API call which has a limited number of requests. To access the method for testing, set the ingredients param = "" or "eggs,flour,butter" and it will output stored values.
 
-# def getRecipeByIngredients(ingredients):
-#     params = {
-#             'fillIngredients': False,
-#             'ingredients': ingredients,
-#             'limitLicense': False,
-#             'number': 5,
-#             'ranking': 1
-#         }
+def get_recipies_by_ingredients(ingredients: str, number: int = 5, limitLicense: bool = False, ranking: int = 1, ignorePantry: bool = False):
+    query = {
+        "ingredients":ingredients,
+        "number":number,
+        "limitLicense":limitLicense,
+        "ranking":ranking,
+        "ignorePantry":ignorePantry
+    }
     
-#     Spoonacular_API_Key = os.environ['ddb49585b28a4350827703b3ef3b3fd2']
-
-#     url = "https://api.spoonacular.com/recipes/findByIngredients"
+    test_query = { 
+        "ingredients":"eggs,flour,butter", 
+        "number":5,
+        "limitLicense":False,
+        "ranking":1,
+        "ignorePantry":False
+    }
     
-    
+    if ingredients == "" or ingredients == test_query["ingredients"]:
+        test_output = open("APITestOutput.txt")
+        test_result_data = test_output.read()
+        test_output.close()
+        
+        print(test_result_data)
+        return test_result_data
 
+    else:
+        url = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients"
 
+        headers = {
+            "x-rapidapi-key": "0969f52154mshf5d39e0b2d8cbf0p1af05bjsn7ac99719ef08",
+            "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
+        }   
 
-# r = requests.get('https://api.spoonacular.com/recipes/findByIngredients', auth=('02-pizzas.trips@icloud.com', 'vihzib-6wewnu-kyPwuw'))
-
-# print(r.status_code)
-
-
-# conn = http.client.HTTPSConnection("https://api.spoonacular.com")
-
-# headers = { 'ddb49585b28a4350827703b3ef3b3fd2': "https://api.spoonacular.com" }
-
-# conn.request("GET", "https://api.spoonacular.com/recipes/findByIngredients", headers=headers)
-
-# res = conn.getresponse()
-# data = res.read()
-
-# print(data.decode("utf-8"))
+        response = requests.get(url, headers=headers, params=query)
+        
+        print(response.json())
+        return response.json()
