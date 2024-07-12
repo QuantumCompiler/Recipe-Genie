ff5dc999d26ea691d3ba4a853bd612a04d3fa7fc
QuantumCompiler <tjlarrechea@gmail.com>
2024-07-11 17:05:17 -0700

    Frontend Tests Update

--- recipe-genie/src/Tests/test.test.js
+++ recipe-genie/src/Tests/test.test.js
@@ -1,6 +1,6 @@
+import '@testing-library/jest-dom';
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom';
 import { BrowserRouter } from 'react-router-dom';
 import CreateRecipe from '../JS/Components/CreateNewRecipe';
 import Dashboard from '../JS/Components/Dashboard';
